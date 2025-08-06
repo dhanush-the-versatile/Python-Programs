6e47543b9b46e12ab4e6d4e091f3c8edc0ccaadd
Amara Dhanush Kumar <amaradhanushkumar@gmail.com>
2025-08-06 11:28:34 +0530

    Update 2d.py

--- 2d.py
+++ 2d.py
@@ -1,2 +1,8 @@
 #Python Program to display all prime numbers within an interval
-n=int(input("Enter the Range ie.., from zero to (upper limit) : "))
+num = int(input("Enter a Number: "))
+for n in range(2, num):
+    for i in range(2, n):
+        if n % i == 0:
+            break
+    else:
+        print(n)
