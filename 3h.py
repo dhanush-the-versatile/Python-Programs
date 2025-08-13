75213a676af2015e7e09773c7fc47aca272be1a5
Amara Dhanush Kumar <amaradhanushkumar@gmail.com>
2025-08-13 12:08:22 +0530

    Add files via upload

---
+++ 3h.py
@@ -0,0 +1,7 @@
+'''python script to take input as amount in rupees R and find out the least
+number of notes N that can be possible to store in a Wallet.'''
+amount=int(input("Enter the amount"))
+o=amount%10
+t=
+h
+th
