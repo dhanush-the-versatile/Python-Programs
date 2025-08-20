fe45166953c70dc3a0243431700fda6cfc9408de
Amara Dhanush Kumar <amaradhanushkumar@gmail.com>
2025-08-20 09:46:12 +0530

    Update 3h.py

--- 3h.py
+++ 3h.py
@@ -1,7 +1,10 @@
 '''python script to take input as amount in rupees R and find out the least
 number of notes N that can be possible to store in a Wallet.'''
 amount=int(input("Enter the amount"))
-o=amount%10
-t=
-h
-th
+notes=0
+denominations=[2000,500,200,100,50,20,10]
+for note in denominations:
+    count=amount//note
+    notes+=count
+    amount=amount%note
+print("Minimum number of notes required:",notes)
