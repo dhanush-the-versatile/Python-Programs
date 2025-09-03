ec3ef666e29b15e027ddd4467545d8783c9d75bf
Amara Dhanush Kumar <amaradhanushkumar@gmail.com>
2025-09-03 10:19:13 +0530

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,12 @@
+# Python script to print the pattern
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end='  ')
+    print()
+"""
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+"""
---
+++ 4c.py
@@ -0,0 +1,12 @@
+# Python script to print the pattern
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(i, end=' ')
+    print()
+"""
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+"""
